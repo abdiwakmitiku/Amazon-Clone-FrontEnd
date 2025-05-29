5a51f9dc6fb19cf3ca556f4a47dd3bd03840d915
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-29 21:02:09 +0300

    Fix case-sensitive import path for Netlify

--- src/Router.jsx
+++ src/Router.jsx
@@ -17,7 +17,7 @@ const stripePromise = loadStripe(
 
 function Routing() {
   return (
-    <Router basename="/Amazon-Clone-Evangadi">
+    <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
