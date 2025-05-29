38c1ea515a17b3caf4229def193fce54b420f6ce
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-29 18:21:52 +0300

    Deploy FrontEnd

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-28ad8/us-central1/api",
+
+         // Local Instance of Firebase Functions
+    // baseURL: "http://127.0.0.1:5001/clone-28ad8/us-central1/api",
+
+        // Deployed Version Of Amazon Server On Render.Com
+   baseURL:"https://amazon-clone-backend-vly0.onrender.com",
 })
 
 export {axiosInstance}
